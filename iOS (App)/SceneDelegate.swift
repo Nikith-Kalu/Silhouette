74f3f46af70eaa4ee0bf9bcd57553c13f0b72a26
Nikith Kalu <nikithkalu@gmail.com>
2022-08-03 21:54:37 +1000

    Initial Commit

---
+++ iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Nikith Kalu on 3/8/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
